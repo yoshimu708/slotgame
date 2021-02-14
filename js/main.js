1923b13b27f85e7bb8d5254dbe2baa32b8c1d55b
yoshimu708 <73528183+yoshimu708@users.noreply.github.com>
2021-02-15 01:17:06 +0900

    javaでのmain要素生成

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-'use strict'
+'use strict';
+
+{
+  class Panel{
+    constructor(){
+      const section=document.createElement('section');
+      section.classList.add('panel');
+
+      this.img=document.createElement('img');
+      this.img.src= 'img/seven.png';
+
+      this.btn = document.createElement('div');
+      this.btn.textContent = 'STOP';
+      this.btn.classList.add('btn');
+
+      section.appendChild(this.img);
+      section.appendChild(this.btn);
+
+      const main= document.querySelector('main');
+      main.appendChild(section);
+    }
+  }
+
+  const panels=[
+    new Panel(),
+    new Panel(),
+    new Panel()
+  ];
+
+}
