9a9f40ad2026a99ca2d964634a59dbc27664e24b
yoshimu708 <73528183+yoshimu708@users.noreply.github.com>
2021-02-15 01:44:40 +0900

    結果の判定

--- js/main.js
+++ js/main.js
@@ -7,11 +7,22 @@
       section.classList.add('panel');
 
       this.img=document.createElement('img');
-      this.img.src= 'img/seven.png';
+      this.img.src= this.getRandomImage();
+
+      this.timeoutId= undefined;
 
       this.btn = document.createElement('div');
       this.btn.textContent = 'STOP';
       this.btn.classList.add('btn');
+      this.btn.addEventListener('click',()=>{
+        clearTimeout(this.timeoutId);
+
+        panelsLeft--;
+
+        if(panelsLeft === 0){
+          checkResult();
+        }
+      });
 
       section.appendChild(this.img);
       section.appendChild(this.btn);
@@ -31,9 +42,29 @@
 
     spin(){
       this.img.src = this.getRandomImage();
-      setTimeout(()=>{
+      this.timeoutId = setTimeout(()=>{
         this.spin()
       },50);
+    }
+
+    isUnmatched(p1,p2){
+      return this.img.src !== p1.img.src && this.img.src !== p2.img.src;
+    }
+
+    unmatch(){
+      this.img.classList.add('unmatched');
+    }
+  }
+
+  function checkResult(){
+    if(panels[0].isUnmatched(panels[1],panels[2])){
+      panels[0].unmatch();
+    }
+    if(panels[1].isUnmatched(panels[0],panels[2])){
+      panels[1].unmatch();
+    }
+    if(panels[2].isUnmatched(panels[0],panels[1])){
+      panels[2].unmatch();
     }
   }
 
@@ -42,6 +73,8 @@
     new Panel(),
     new Panel()
   ];
+
+  let panelsLeft =3;
 
   const spin = document.getElementById('spin');
   spin.addEventListener('click',()=>{
